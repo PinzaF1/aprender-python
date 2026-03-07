313dd3c507bd3a02e781e4534fba548876f1bf16
Carlos Pinza <carlosdanielpinzadiaz@gmail.com>
2026-03-06 23:50:14 -0500

    Agregar script de variables básicas en Python

---
+++ 02_variables.py
@@ -0,0 +1,15 @@
+# 02_variables.py - Aprendiendo variables en Python
+
+# Las variables almacenan datos que podemos usar después
+nombre = "Carlos"
+edad = 25
+altura = 1.75
+
+# Imprimir las variables
+print("Hola, mi nombre es", nombre)
+print("Tengo", edad, "años")
+print("Mido", altura, "metros")
+
+# Cambiar el valor de una variable
+edad = edad + 1
+print("El próximo año tendré", edad, "años")
