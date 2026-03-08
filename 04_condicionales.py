3ac888fd4d3079f7d2632e1b1b70111e1f9a87c0
Carlos Pinza <carlosdanielpinzadiaz@gmail.com>
2026-03-07 23:26:24 -0500

    Agregar script de condicionales básico en Python

---
+++ 04_condicionales.py
@@ -0,0 +1,23 @@
+# 04_condicionales.py - Aprendiendo condicionales en Python
+
+# Los condicionales permiten tomar decisiones
+edad = 18
+
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+# Condicional con elif
+nota = 85
+
+if nota >= 90:
+    print("Excelente")
+elif nota >= 80:
+    print("Muy bien")
+elif nota >= 70:
+    print("Bien")
+else:
+    print("Necesitas mejorar")
+
+print("¡Fin del programa!")
