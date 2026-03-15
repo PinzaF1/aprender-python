6872c8af7df11b6eae2d5e9b6713e3cb6db5e8f6
Carlos Pinza <carlosdanielpinzadiaz@gmail.com>
2026-03-14 23:36:25 -0500

    Add nuevo archivo de ejemplo

---
+++ 09_nuevo_archivo.py
@@ -0,0 +1,7 @@
+"""Ejemplo de nuevo archivo agregado al repositorio.
+
+Este archivo solo sirve de demostración para un commit adicional.
+"""
+
+if __name__ == "__main__":
+    print("¡Hola desde el nuevo archivo!")
